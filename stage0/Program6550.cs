529fd732c419a5d7c80f7ae32aee4f15be8e86fc
malki-b <rivkib718@gmail.com>
2024-10-31 17:18:35 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program6550.cs
@@ -0,0 +1,18 @@
+﻿    partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcom6550();
+        Welcom4998();
+                Console.ReadKey();
+    }
+
+    private static void Welcom6550()
+    {
+        Console.Write("Enter your name ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+    static partial void Welcom4998();
+  
+}

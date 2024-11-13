7f93a9019d5d03051def9f8c9ea0b0089c1d4752
chaya295 <c0556700295@gmail.com>
2024-11-13 21:28:45 +0200

    סוף פרק 7

---
+++ DalList/Config.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Dal;
+
+//internal static class Config
+//{
+//    internal const int startCallIdId = 1000;
+//    private static int NextCallId = startCallIdId;
+//    internal static int NextCallId { get => NextCallId++; }
+//    //...
+
+//    internal static DateTime Clock { get; set; } = DateTime.Now;
+//    //...
+
+//    internal static void Reset()
+//    {
+//        NextCallId = startCallIdId;
+//        //...
+//        Clock = DateTime.Now;
+//        //...
+//    }
+//}

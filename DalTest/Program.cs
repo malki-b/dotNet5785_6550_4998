f3e4c08a29dea659755f096703ad787c70c5cddc
סימן טוב חיה <c0556700295@gmail,co>
2024-11-25 18:45:41 +0200

    progrem1

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace DalTest
+﻿using Dal;
+using DalApi;
+
+namespace DalTest
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+
+        private static IVolunteer? s_dalVolunteer = new VolunteerImplementation(); //stage 1
+        private static ICall? s_dalCall = new CallImplementation(); //stage 1
+        private static IAssignment? s_dalAssignment = new AssignmentImplementation(); //stage 1
+        private static IConfig? s_dalConfig = new ConfigImplementation(); //stage 1
+
+        private enum MainMenu
         {
-            Console.WriteLine("Hello, World!");
+            Exit,
+            VolunteerMenu,
+            CallMenu,
+            AssignmentMenu,
+            InitializeData,
+            DisplayAllData,
+            ConfigMenu,
+            ResetDatabase
+        }
+        private enum Option
+        {
+            Exit,
+            Create,
+            Read,
+            ReadAll,
+            UpDate,
+            Delete,
+            DeleteAll
+        }
+        private enum ConfigSubmenu
+        {
+            Exit,
+            AdvanceClockByMinute,
+            AdvanceClockByHour,
+            AdvanceClockByDay,
+            AdvanceClockByMonth,
+            AdvanceClockByYear,
+            DisplayClock,
+            ChangeClockOrRiskRange,
+            DisplayConfigVar,
+            Reset
         }
     }
+
+    
 }
+   
+}
+

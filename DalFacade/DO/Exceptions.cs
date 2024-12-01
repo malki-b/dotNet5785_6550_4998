2182df5073ea765542a15729bbbc788f9f591802
malki-b <rivkib718@gmail.com>
2024-12-01 21:26:16 +0200

    התחלת פרק 9

---
+++ DalFacade/DO/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace DO;
+
+
+[Serializable]
+public class DalDoesNotExistException : Exception
+{
+    public DalDoesNotExistException(string? message) : base(message) { }
+}
+[Serializable]
+public class DalAlreadyExistsException : Exception// האובייקט כבר קיים
+{
+    public DalAlreadyExistsException(string? message) : base(message) { }
+}
+public class DalDeletionImpossible : Exception// המחיקה בלתי אפשרית"
+{
+    public DalDeletionImpossible(string? message) : base(message) { }
+}

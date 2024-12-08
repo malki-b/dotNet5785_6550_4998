faf2fd67a21bcb2a2a7815875b1d5132ce8f5826
malki-b <rivkib718@gmail.com>
2024-12-08 20:58:47 +0200

    Continued corrections to chapters one and two1

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -6,12 +6,12 @@ internal static class Config
     internal const int startCallId = 1000;
     private static int nextCallId = startCallId;
     internal static int NextCallId { get => nextCallId++; }
-    //...
+
     internal const int startAssignmentId = 1000;
     private static int nextAssignmentId = startAssignmentId;
     public static int NextAssignmentId { get => nextAssignmentId++; }
     internal static DateTime Clock { get; set; } = new DateTime(2024, 12, 08, 19, 59, 30);
-    //...
+ 
     internal static TimeSpan RiskRange { get; set; } = TimeSpan.FromHours(1);
     internal static void Reset()
     {

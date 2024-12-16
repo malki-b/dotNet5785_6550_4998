75878c114820fca4676b95679d6b3947ea689aee
סימן טוב חיה <c0556700295@gmail,co>
2024-12-16 15:15:11 +0200

    פרק 6

--- DalXml/AssignmentImplementation.cs
+++ DalXml/AssignmentImplementation.cs
@@ -3,6 +3,43 @@
 namespace Dal;
 using DalApi;
 using DO;
+using System;
+using System.Collections.Generic;
+
 internal class AssignmentImplementation : IAssignment
 {
+    public void Create(Assignment item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Assignment? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Assignment? Read(Func<Assignment, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Assignment> ReadAll(Func<Assignment, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Assignment item)
+    {
+        throw new NotImplementedException();
+    }
 }

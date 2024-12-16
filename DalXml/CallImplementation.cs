1fee32a1793622f958ddb4906933354672478c46
malki-b <rivkib718@gmail.com>
2024-12-16 15:54:23 +0200

    CallImplementation

--- DalXml/CallImplementation.cs
+++ DalXml/CallImplementation.cs
@@ -10,36 +10,61 @@ internal class CallImplementation : ICall
 {
     public void Create(Call item)
     {
-        throw new NotImplementedException();
+        List<Call> Calls = XMLTools.LoadListFromXMLSerializer<Call>(Config.s_calls_xml);
+        int newId = Config.NextCallId;
+        Call newCall = item with { Id = newId };
+        Calls.Add(newCall);
+        XMLTools.SaveListToXMLSerializer(Calls, Config.s_courses_xml);
+        Console.WriteLine($"your id is {newId}");
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<Call> Calls = XMLTools.LoadListFromXMLSerializer<Call>(Config.s_calls_xml);
+        if (Calls.RemoveAll(it => it.Id == id) == 0)
+            throw new DalDoesNotExistException($"Call with ID={id} does Not exist");
+        XMLTools.SaveListToXMLSerializer(Calls, Config.s_calls_xml);
     }
 
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        XMLTools.SaveListToXMLSerializer(new List<Call>(), Config.s_calls_xml);
     }
 
     public Call? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Call> Calls = XMLTools.LoadListFromXMLSerializer<Call>(Config.s_calls_xml);
+        return Calls.FirstOrDefault(item => item.Id == id); //stage 2
     }
 
     public Call? Read(Func<Call, bool> filter)
     {
-        throw new NotImplementedException();
+        List<Call> Calls = XMLTools.LoadListFromXMLSerializer<Call>(Config.s_calls_xml);
+        return Calls.FirstOrDefault(filter);
     }
 
     public IEnumerable<Call> ReadAll(Func<Call, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<Call> Calls = XMLTools.LoadListFromXMLSerializer<Call>(Config.s_calls_xml);
+        if (filter == null)
+            Calls.Select(item => item);
+        else
+            Calls.Where(filter);
     }
 
     public void Update(Call item)
     {
-        throw new NotImplementedException();
+        List<Call> Calls = XMLTools.LoadListFromXMLSerializer<Call>(Config.s_calls_xml);
+        if (Calls.RemoveAll(it => it.Id == item.Id) == 0)
+            throw new DalDoesNotExistException($"Call with ID={item.Id} does Not exist");
+        Calls.Add(item);
+        XMLTools.SaveListToXMLSerializer(Calls, Config.s_calls_xml);
     }
+
+
+
+
+   
+
+
 }
--- DalXml/VolunteerImplementation.cs
+++ DalXml/VolunteerImplementation.cs
@@ -42,4 +42,49 @@ internal class VolunteerImplementation : IVolunteer
     {
         throw new NotImplementedException();
     }
+
+
+
+
+
+
+
+    static Student getStudent(XElement s)
+    {
+        return new DO.Student()
+        {
+            Id = s.ToIntNullable("Id") ?? throw new FormatException("can't convert id"),
+            Name = (string?)s.Element("Name") ?? "",
+            Alias = (string?)s.Element("Alias") ?? null,
+            IsActive = (bool?)s.Element("IsActive") ?? false,
+            //CurrentYear = s.ToEnumNullable<Year>("CurrentYear") ?? Year.FirstYear,
+            BirthDate = s.ToDateTimeNullable("BirthDate"),
+            RegistrationDate = s.ToDateTimeNullable("RegistrationDate")
+        };
+    }
+    public Student? Read(int id)
+    {
+        XElement? studentElem =
+    XMLTools.LoadListFromXMLElement(Config.s_students_xml).Elements().FirstOrDefault(st => (int?)st.Element("Id") == id);
+        return studentElem is null ? null : getStudent(studentElem);
+    }
+
+    public Student? Read(Func<Student, bool> filter)
+    {
+        return XMLTools.LoadListFromXMLElement(Config.s_students_xml).Elements().Select(s => getStudent(s)).FirstOrDefault(filter);
+    }
+
+    public void Update(Student item)
+    {
+        XElement studentsRootElem = XMLTools.LoadListFromXMLElement(Config.s_students_xml);
+
+        (studentsRootElem.Elements().FirstOrDefault(st => (int?)st.Element("Id") == item.Id)
+        ?? throw new DO.DalDoesNotExistException($"Student with ID={item.Id} does Not exist"))
+                .Remove();
+
+        studentsRootElem.Add(new XElement("Student", createStudentElement(item)));
+
+        XMLTools.SaveListToXMLElement(studentsRootElem, Config.s_students_xml);
+    }
+
 }

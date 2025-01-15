fff252d195320aeff42a10981b1090d993db879b
malki-b <rivkib718@gmail.com>
2025-01-15 21:18:14 +0200

    פרק 7 ג

---
+++ BL/BO/CallInProgress .cs
@@ -0,0 +1,15 @@
+﻿using Helpers;
+namespace BO;
+public class StudentInCourse
+{
+    public int StudentId { get; init; }
+    public required Tuple<int, string, string> Course
+    {
+        get; init;
+
+    public Year? InYear { get; init; }
+    public SemesterNames? InSemester { get; init; }
+    public double? Grade { get; set; }
+    public int? Credits { get; init; }
+    public override string ToString() => this.ToStringProperty();
+}
---
+++ BL/BO/Volunteer.cs
@@ -0,0 +1,14 @@
+﻿using Helpers;
+namespace BO;
+public class Student
+{
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public string? Alias { get; set; }
+    public bool IsActive { get; set; }
+    DateTime? BirthDate { get; set; }
+    public DateTime RegistrationDate { get; init; }
+    Year CurrentYear { get; init; } //(BO.Year)(ClockManager.Now.Year - registrationDate?.Year!);
+    public override string ToString() => this.ToStringProperty();
+}
+

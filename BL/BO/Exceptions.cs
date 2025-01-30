edd9d04f6124bee9469696dba3a7e699295c0c05
סימן טוב חיה <c0556700295@gmail,co>
2025-01-30 14:54:30 +0200

    exception

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -2,26 +2,21 @@
 
 namespace BO;
 
-//namespace DO;
 
-
-internal class Exceptions
+[Serializable]
+public class BlDoesNotExistException : Exception
 {
-
-    [Serializable]
-    public class BlDoesNotExistException : Exception
-    {
-        public BlDoesNotExistException(string? message) : base(message) { }
-        public BlDoesNotExistException(string message, Exception innerException)
-                    : base(message, innerException) { }
-    }
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string message, Exception innerException)
+                : base(message, innerException) { }
+}
    
 
-    [Serializable]
-    public class BlNullPropertyException : Exception
-    {
-        public BlNullPropertyException(string? message) : base(message) { }
-    }
-
+[Serializable]
+public class BlNullPropertyException : Exception
+{
+    public BlNullPropertyException(string? message) : base(message) { }
 }
+
+
 

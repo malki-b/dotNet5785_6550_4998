5525e1da2cb50060ff47f2942414d2e0d939d824
סימן טוב חיה <c0556700295@gmail,co>
2025-03-11 12:33:36 +0200

    AdminImplementation

--- BL/BlApi/IAdmin.cs
+++ BL/BlApi/IAdmin.cs
@@ -10,8 +10,8 @@ public interface IAdmin
 {
     void InitializeDB();
     void ResetDB();
-    int GetMaxRange();
-    void SetMaxRange(int maxRange);
+    TimeSpan GetMaxRange();
+    void SetMaxRange(TimeSpan maxRange);
     DateTime GetClock();
     void ForwardClock(BO.TimeUnit unit);
 }
--- BL/BlImplementation/AdminImplementation.cs
+++ BL/BlImplementation/AdminImplementation.cs
@@ -10,7 +10,26 @@ internal class AdminImplementation : IAdmin
     private readonly DalApi.IDal _dal = DalApi.Factory.Get;
     public void ForwardClock(TimeUnit unit)
     {
-        throw new NotImplementedException();
+        switch (unit)
+        {
+            case TimeUnit.Minute:
+                ClockManager.UpdateClock(ClockManager.Now.AddMinutes(1));
+                break;
+            case TimeUnit.Hour:
+                ClockManager.UpdateClock(ClockManager.Now.AddHours(1));
+                break;
+            case TimeUnit.Day:
+                ClockManager.UpdateClock(ClockManager.Now.AddDays(1));
+                break;
+            case TimeUnit.Month:
+                ClockManager.UpdateClock(ClockManager.Now.AddMonths(1));
+                break;
+            case TimeUnit.Year:
+                ClockManager.UpdateClock(ClockManager.Now.AddYears(1));
+                break;
+            default:
+                throw new ArgumentException("Invalid TimeUnit", nameof(unit));
+        }
     }
 
     public DateTime GetClock()
@@ -18,27 +37,28 @@ internal class AdminImplementation : IAdmin
         ClockManager.UpdateClock(ClockManager.Now.AddMinutes(1));
         return ClockManager.Now;    
     }
-
-    public int GetMaxRange()
+    public void SetMaxRange(TimeSpan riskTimeRange)
     {
-        throw new NotImplementedException();
+        _dal.Config.RiskRange = riskTimeRange;
+    }
+    public TimeSpan GetMaxRange()
+    {
+        return _dal.Config.RiskRange;
     }
 
     public void InitializeDB()
     {
+        _dal.Config.Reset();
         DalTest.Initialization.Do();
         ClockManager.UpdateClock(ClockManager.Now);
     }
 
     public void ResetDB()
     {
-        _dal.ResetDB();
+        _dal.Config.Reset();
+        //
         GetClock();
     }
 
-    public void SetMaxRange(int maxRange)
-    {
-
-        throw new NotImplementedException();
-    }
+    
 }

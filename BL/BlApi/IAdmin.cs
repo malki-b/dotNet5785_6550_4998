988a67081b29ff6082486b4184712483b867cb41
סימן טוב חיה <c0556700295@gmail,co>
2025-04-28 18:30:56 +0300

    finish 1

--- BL/BlApi/IAdmin.cs
+++ BL/BlApi/IAdmin.cs
@@ -14,4 +14,12 @@ public interface IAdmin
     void SetMaxRange(TimeSpan maxRange);
     DateTime GetClock();
     void ForwardClock(BO.TimeUnit unit);
+
+    #region Stage 5
+    void AddConfigObserver(Action configObserver);
+    void RemoveConfigObserver(Action configObserver);
+    void AddClockObserver(Action clockObserver);
+    void RemoveClockObserver(Action clockObserver);
+    #endregion Stage 5
+
 }
--- BL/Helpers/AdminManager.cs
+++ BL/Helpers/AdminManager.cs
@@ -83,7 +83,7 @@ internal static class AdminManager //stage 4
             AdminManager.UpdateClock(AdminManager.Now); //stage 5 - needed for update PL
             AdminManager.MaxRange = AdminManager.MaxRange; //stage 5 - needed for update PL
         }
-
+    }
     #endregion Stage 4
 
         #region Stage 7 base
